2d31c957ced2a66178073ed7d7f8682a7f2edf65
Icaro Freire <icarovf@usp.br>
2021-07-08 06:48:24 -0300

    updates

--- src/game.asm
+++ src/game.asm
@@ -1,9 +1,56 @@
 .segment "HEADER"
+.byte "NES"
+.byte $1a
+.byte $02
+.byte $01
+.byte %00000000
+.byte $00
+.byte $00
+.byte $00
+.byte $00
+.byte $00, $00, $00, $00, $00
 
 .segment "ZEROPAGE"
 
 .segment "STARTUP"
+Reset:
+	SEI	; disable interruptions
+	CLD	; disable decimal mode
+
+vblankwait:
+	BIT $2002
+	BPL vblankwait
+
+	; clears RAM before game starts
+	LDX #$00
+clrmem:
+	LDA #$00
+	STA $0000, X
+	STA $0100, X
+	STA $0200, X
+	STA $0300, X
+	STA $0400, X
+	STA $0500, X
+	STA $0600, X
+	STA $0700, X
+	INX
+	BEQ clrmem
+
+vblankwait2:
+	BIT $2002
+	BPL vblankwait2
+
+	LDA #%01000001
+	STA $2001
+
+Forever:
+	JMP	Forever
+
+MNI:
+	RTI
 
 .segment "VECTORS"
+	.word MNI
+	.word Reset
 
 .segment "CHARS"
